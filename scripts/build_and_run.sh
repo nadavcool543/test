5d66affe09ca735eb572a9fe002dce044c7d1a7c
nadav <nadavcool543@gmail.com>
2024-11-21 20:13:15 +0200

    changes

--- scripts/build_and_run.sh
+++ scripts/build_and_run.sh
@@ -8,5 +8,5 @@ docker build -t backend  ./backend/.
 docker build -t db ./db/.
 
 docker run -d --name frontend -p 8080:80 --network frontend_net frontend
-docker run -d --name backend -p 3000:3000 -e DB_HOST=db -e DB_PORT=3306 -e DB_USER=root -e DB_PASSWORD=${{ env.DB_PASSWORD }} -e DB_NAME=mydatabase --network frontend_net --network backend_net backend
-docker run -d --name db -e MYSQL_ROOT_PASSWORD=${{ env.DB_PASSWORD }} -e MYSQL_DATABASE=mydatabase --network backend_net db
+docker run -d --name backend -p 3000:3000 -e DB_HOST=db -e DB_PORT=3306 -e DB_USER=root -e DB_PASSWORD=${{ DB_PASSWORD }} -e DB_NAME=mydatabase --network frontend_net --network backend_net backend
+docker run -d --name db -e MYSQL_ROOT_PASSWORD=${{ DB_PASSWORD }} -e MYSQL_DATABASE=mydatabase --network backend_net db
